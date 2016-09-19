270d1c4a0b17ca089e1db34cde451b09240a13e9
Legodev <legodevgit@mailbox.org>
2016-09-19 03:08:48 +0200

    A3EX: Switch to CMake

--- Arma 3 Extensions/Redux Extension/Source/build.bat
+++
@@ -1,3 +0,0 @@
-g++ -std=c++11 -Wall -g -c uuid.cpp
-g++ -std=c++11 -IC:\mingw-w64\i686-5.4.0-posix-dwarf-rt_v5-rev0\mingw32\include -Wall -g -c redex.cpp
-g++ -static -shared -std=c++11 -Wall -g -o "D:\Steam\steamapps\common\Arma 3\redex.dll" redex.o uuid.o -lrpcrt4
