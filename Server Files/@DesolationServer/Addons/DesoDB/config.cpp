f55d7ef091661f7bba27efdcd95c222181c0273a
ryancheek@hush.com <ryancheek@hush.com>
2016-09-22 16:58:57 -0400

    Created DesoDB
    DesoDB is the custom databasing plugin that legodev is creating

---
+++ Server Files/@DesolationServer/Addons/DesoDB/config.cpp
@@ -0,0 +1,29 @@
+class CfgPatches
+{
+	class DesoDB {};
+};
+class Plugins
+{
+	class DesoDB
+	{
+		name = "DesoDB";
+		desc = "Desolation Redux's custom Database connection plugin";
+		tag = "DB";
+	};
+}
+class CfgFunctions
+{
+	class DB
+	{
+		class Database 
+		{
+			file = "DesoDB\Database";
+			isserver = 1;
+		};
+		class Functions 
+		{
+			file = "DesoDB\Functions";
+			isserver = 1;
+		};
+	};
+};
