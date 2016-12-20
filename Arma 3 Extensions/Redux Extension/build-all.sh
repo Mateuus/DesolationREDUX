d7d5ac8dcaaf60d170bd17f963ca042fa217e656
Legodev <legodevgit@mailbox.org>
2016-12-20 17:08:15 +0100

    A3EX: added definition to build retail libary

--- Arma 3 Extensions/Redux Extension/build-all.sh
+++ Arma 3 Extensions/Redux Extension/build-all.sh
@@ -4,26 +4,26 @@ cd build
 ####
 mkdir -p 32bit-linux
 cd 32bit-linux
-cmake ../../ -DBITVALUE=32
+cmake ../../ -DBITVALUE=32 -DCMAKE_BUILD_TYPE=Release
 make
 cd ..
 ####
 mkdir -p 64bit-linux
 cd 64bit-linux
-cmake ../../ -DBITVALUE=64
+cmake ../../ -DBITVALUE=64 -DCMAKE_BUILD_TYPE=Release
 make
 cd ..
 ####
 mkdir -p 32bit-windows
 cd 32bit-windows
-/opt/mxe/usr/bin/i686-w64-mingw32.static.posix-cmake ../../ -DBITVALUE=32
+/opt/mxe/usr/bin/i686-w64-mingw32.static.posix-cmake ../../ -DBITVALUE=32 -DCMAKE_BUILD_TYPE=Release
 make
 cp -f libredex.dll ../../Binaries
 cd ..
 ####
 mkdir -p 64bit-windows
 cd 64bit-windows
-/opt/mxe/usr/bin/x86_64-w64-mingw32.static.posix-cmake ../../ -DBITVALUE=64
+/opt/mxe/usr/bin/x86_64-w64-mingw32.static.posix-cmake ../../ -DBITVALUE=64 -DCMAKE_BUILD_TYPE=Release
 make
 cp -f libredex_x64.dll ../../Binaries
 cd ..
