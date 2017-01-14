4fa61c60f9a44362137823cf4b886eaeb6ad90e4
BigBenMOTO <bensaccord@hotmail.com>
2017-01-14 00:52:16 -0800

    added dsr_stockade_tower
    Added all files for dsr_stockade_tower

--- Client Files/@Desolation/Addons/dsr_objects/config.cpp
+++ Client Files/@Desolation/Addons/dsr_objects/config.cpp
@@ -163,6 +163,17 @@ class CfgVehicles
 		editorSubcategory = "DSR_Objects";
 		mass = 150;
 	};
+	class dsr_stockade_tower: DSR_objects_base
+	{
+		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_Axe_F.jpg";
+		scope = 2;
+		scopeCurator = 2;
+		displayName = "Stockade Tower";
+		model = "\dsr_objects\stockade_tower\dsr_stockade_tower.p3d";
+		icon = "iconObject_4x1";
+		editorSubcategory = "DSR_Objects";
+		mass = 150;
+	};
 	class dsr_objects_workbench: DSR_objects_base
 	{
 		displayName = "Workbench";
