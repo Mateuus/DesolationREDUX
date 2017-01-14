3d43c281e5118fed986501293e9965f81ed343d6
BigBenMOTO <bensaccord@hotmail.com>
2017-01-14 00:36:36 -0800

    updated p3d and rvmat for wooden storage
    Updated rvmat and p3d's for small and large wooden storage to fix shadow
    issues.

--- Client Files/@Desolation/Addons/dsr_weapons/srifle/dmr/config.cpp
+++ Client Files/@Desolation/Addons/dsr_weapons/srifle/dmr/config.cpp
@@ -21,6 +21,20 @@ class SlotInfo;
 class MuzzleSlot;
 class CowsSlot;
 class PointerSlot;
+/*
+ * Desolation Redux
+ * http://desolationredux.com/
+ * © 2016 Desolation Dev Team
+ * 
+ * This work is licensed under the Arma Public License Share Alike (APL-SA) + Bohemia monetization rights.
+ * To view a copy of this license, visit:
+ * https://www.bistudio.com/community/licenses/arma-public-license-share-alike/
+ * https://www.bistudio.com/monetization/
+ */
+
+/*
+	Quick N Dirty
+*/
 class UnderBarrelSlot;
 class CfgWeapons
 {
